ad6bdaeba3e059aa31326d7ea6be2de29b44111c
Zayidu Ansari <zayidu11@gmail.com>
2020-08-02 22:04:32 +0400

    Added Product API  CRUD

--- api/urls.py
+++ api/urls.py
@@ -20,5 +20,6 @@ from .views import home
 
 urlpatterns = [
     path('', home, name='api.home'),
-    path('category/', include('api.category.urls'))
+    path('category/', include('api.category.urls')),
+    path('product/', include('api.product.urls'))
 ]
