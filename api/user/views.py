1c6ae43bc5279875e650865e83b97c79d97ed062
Zayidu Ansari <zayidu11@gmail.com>
2020-08-04 11:19:32 +0400

    Resolved the issues on SignUp API

--- api/user/views.py
+++ api/user/views.py
@@ -17,7 +17,7 @@ import re
 # def generate_session_token(length=10):
 #     return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)], [str(i) for i in range(10)]) for _ in range(length))
 def generate_session_token(length=10):
-    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]) for _ in range(length))
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)], [str(i) for i in range(10)]) for _ in range(length))
 
 # signin
 
